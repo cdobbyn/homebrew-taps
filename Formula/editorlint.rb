e2e4a17a94dc8e907699e5c9e7dd98817f3428cf
Chris Dobbyn <8264498+cdobbyn@users.noreply.github.com>
2025-08-26 09:26:38 -0700

    fix: update SHA256s for 1.3.9 release binaries

--- Formula/editorlint.rb
+++ Formula/editorlint.rb
@@ -7,20 +7,20 @@ class Editorlint < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/cdobbyn/editorlint/releases/download/1.3.9/editorlint_v1.3.9_darwin_arm64.tar.gz"
-      sha256 "darwin_arm64_sha256_placeholder"
+      sha256 "9cece0ffe7cad27ff7fcb1c337fe9443ae5b5bf872f179491e00ecf629d6db75"
     else
       url "https://github.com/cdobbyn/editorlint/releases/download/1.3.9/editorlint_v1.3.9_darwin_amd64.tar.gz"
-      sha256 "darwin_amd64_sha256_placeholder"
+      sha256 "36d79bf043239a159aac280e050511da91a44f1b5a91e67b56bb8f4b7dc8dfd2"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/cdobbyn/editorlint/releases/download/1.3.9/editorlint_v1.3.9_linux_arm64.tar.gz"
-      sha256 "linux_arm64_sha256_placeholder"
+      sha256 "905f998166df2ead06e0363f178883651169a5bf7960b476ebd0fa1ce60a23e1"
     else
       url "https://github.com/cdobbyn/editorlint/releases/download/1.3.9/editorlint_v1.3.9_linux_amd64.tar.gz"
-      sha256 "linux_amd64_sha256_placeholder"
+      sha256 "586a4966758dba043d62729c2e3da24f1adea24994c2772a88694e95416da2f4"
     end
   end
 
